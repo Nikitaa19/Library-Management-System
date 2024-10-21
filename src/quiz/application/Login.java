f378efbba8a2f678223d88627ebaf1df9ddab9d2
Nikitaa19 <agarwalnikita064@gmail.com>
2024-10-21 22:41:47 +0530

    second commit

--- src/quiz/application/Login.java
+++ src/quiz/application/Login.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package quiz.application;
 
-/**
- *
- * @author Administrator
- */
 public class Login {
     
 }
