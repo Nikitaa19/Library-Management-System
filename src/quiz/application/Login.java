8020132c8e6e0c410ff6df811e1088c710d61633
Nikitaa19 <agarwalnikita064@gmail.com>
2024-10-22 17:47:31 +0530

    seventh commit

--- src/quiz/application/Login.java
+++ src/quiz/application/Login.java
@@ -1,10 +1,13 @@
 package quiz.application;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Login extends JFrame {
     
     Login() {
+        getContentPane().setBackground(Color.WHITE);
+        
         setSize(1200, 500);
         setLocation(200, 150);
         setVisible(true);
