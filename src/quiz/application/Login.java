f9c5ab79ddbdc8f78d5c6cc925d93ded2c8cb824
Nikitaa19 <agarwalnikita064@gmail.com>
2024-10-22 22:43:01 +0530

    name

--- src/quiz/application/Login.java
+++ src/quiz/application/Login.java
@@ -20,11 +20,11 @@ public class Login extends JFrame {
         heading.setForeground(new Color(30, 144, 254));
         add(heading);
         
-        JLabel heading = new JLabel("Simple Minds");
-        heading.setBounds(750, 60, 300, 45);
-        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 40));
-        heading.setForeground(new Color(30, 144, 254));
-        add(heading);
+        JLabel name = new JLabel("Enter your name");
+        name.setBounds(810, 150, 300, 20);
+        name.setFont(new Font("Monogolian Baiti", Font.BOLD, 18));
+        name.setForeground(new Color(30, 144, 254));
+        add(name);
         
         setSize(1200, 500);
         setLocation(200, 150);
