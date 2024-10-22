d188b2698fdb98aaf3c3847c6bf2d11ef135e3b2
Nikitaa19 <agarwalnikita064@gmail.com>
2024-10-22 17:33:15 +0530

    constructor added

--- src/quiz/application/Login.java
+++ src/quiz/application/Login.java
@@ -2,7 +2,13 @@ package quiz.application;
 
 public class Login {
     
+    Login() {
+        
+    }
+    
     public static void(String[] args) {
+        
+        new Login();
         
     }
     
