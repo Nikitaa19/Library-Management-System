0a018e0e1bdea82e580ed1769daae86f823ab7a3
Nikitaa19 <agarwalnikita064@gmail.com>
2024-10-22 17:43:52 +0530

    frame

--- src/quiz/application/Login.java
+++ src/quiz/application/Login.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 public class Login extends JFrame {
     
     Login() {
+        setSize(1200, 500);
+        setLocation(200, 150);
         setVisible(true);
         
     }
