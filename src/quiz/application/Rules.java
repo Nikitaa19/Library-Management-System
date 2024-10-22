1aa3c06a38c98c8ed55c1794bd2dba769d593ed1
Nikitaa19 <agarwalnikita064@gmail.com>
2024-10-23 00:33:50 +0530

    ruled

--- src/quiz/application/Rules.java
+++ src/quiz/application/Rules.java
@@ -2,10 +2,12 @@ package quiz.application;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class Rules extends JFrame{
+public class Rules extends JFrame implements ActionListener{
     
     String name;
+    JButton start, back;
     
     Rules(String name) {
         
@@ -37,9 +39,33 @@ public class Rules extends JFrame{
         );
         add(rules);
         
+        back = new JButton("Back");
+        back.setBounds(250, 500, 100, 30);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+        
+        start = new JButton("Start");
+        start.setBounds(400, 500, 100, 30);
+        start.setBackground(new Color(30, 144, 254));
+        start.setForeground(Color.WHITE);
+        start.addActionListener(this);
+        add(start);
+        
         setSize(800, 650);
         setLocation(350, 100);
         setVisible(true);
+        
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == start) {
+            
+        } else {
+            setVisible(false);
+            new Login();
+        }
         
     }
     
