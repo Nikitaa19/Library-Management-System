94e14cd52bd057e786ef7a3549dedae620e50c91
Nikitaa19 <agarwalnikita064@gmail.com>
2024-10-22 23:28:59 +0530

    rules added

---
+++ src/quiz/application/Rules.java
@@ -0,0 +1,16 @@
+package quiz.application;
+
+import javax.swing.*;
+
+public class Rules extends JFrame{
+    
+    Rules() {
+        
+    }
+    
+    public static void main(String[] args) {
+        new Rules();
+        
+    }
+    
+}
