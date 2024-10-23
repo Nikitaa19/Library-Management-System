e148b17161831d4465a722416b014ee80899068b
Nikitaa19 <agarwalnikita064@gmail.com>
2024-10-23 17:52:14 +0530

    quiz image

--- src/quiz/application/Quiz.java
+++ src/quiz/application/Quiz.java
@@ -1,8 +1,21 @@
 package quiz.application;
 
-public class Quiz {
+import javax.swing.*;
+import java.awt.*;
+
+public class Quiz extends JFrame {
     
     Quiz() {
+        setBounds(50, 0, 1440, 850);
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Icons/quiz.jpg"));
+        JLabel image = new JLabel(i1);
+        image.setBounds(0, 0, 1440, 392);
+        add(image);
+        
+        setVisible(true);
         
     }
     
